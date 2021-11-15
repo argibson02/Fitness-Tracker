946d563ef18d8cf908e194486767fc9c50402011
Alex Gibson <argibson02@gmail.com>
2021-11-14 16:06:46 -0800

    Added HTML routes. Fixing workout model to match seed data.

--- models/workouts.js
+++ models/workouts.js
@@ -3,38 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    required: "Enter a name for your workout."
-    // c + w
-  },
-  type: {
-    type: String,
-    required: "Enter the type of exercise that you have preformed."
-    // c + w
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  distance: {
-    type: Number,
-    // C
-  },
-  weight: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  duration: {
-    type: Number,
-    required: true
-    // c + w
-  }
+    day: {
+        type: Date,
+        required: true
+    },
+    name: {
+        type: String,
+        required: "Enter a name for your workout."
+        // c + w
+    },
+    type: {
+        type: String,
+        required: "Enter the type of exercise that you have preformed."
+        // c + w
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    distance: {
+        type: Number,
+        // C
+    },
+    weight: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    duration: {
+        type: Number,
+        required: true
+        // c + w
+    }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
