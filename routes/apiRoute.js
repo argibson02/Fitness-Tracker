f41231376f696367061af9614fcdfdd2930d06cc
Alex Gibson <argibson02@gmail.com>
2021-11-14 20:07:26 -0800

    Completed add workout API route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts");
+const mongojs = require("mongojs");
 
 // Finds all workouts
-router.get('/workouts', (req, res) => {
+router.get('/api/workouts', (req, res) => {
     Workout.find({}).sort({ date: -1 })
         .then(dbWorkouts => {
             res.json(dbWorkouts);
@@ -13,34 +14,42 @@ router.get('/workouts', (req, res) => {
 })
 
 
-  // add a workout to an exercise
-  router.put("/workouts/:?", ({ body }, res) => {
-    Workout.updateOne(body)
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+// add a workout to an exercise
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.updateOne(
+        {
+            _id: mongojs.ObjectId(params.id)
+        },
+        {
+            $push: {
+                exercises: body
+            }
+        })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 
 
-  // creates a new workout
-  router.post("/workouts", ({ body }, res) => {
+// creates a new workout
+router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 
 
-  // retrieves workout ranges
-  router.get('/workouts/range', (req, res) => {
+// retrieves workout ranges
+router.get('/api/workouts/range', (req, res) => {
     Workout.find({}).sort({ date: -1 })
         .then(dbWorkouts => {
             res.json(dbWorkouts);
